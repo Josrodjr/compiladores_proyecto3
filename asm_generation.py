49ee8a1e8760727eefcd8088d886d7419b681b17
Josrodjr <Josrodjr@gmail.com>
2020-11-26 04:13:22 -0600

    working

--- asm_generation.py
+++ asm_generation.py
@@ -21,6 +21,10 @@ def main(inter_code):
     markers = {}
     markers = fill_markers(inter_code, markers)
 
+    # generate a list of main and unmain functions inside the text
+    main_list = 0
+    unmain_list = 0
+
     # delimit by enters
     markers['DATA START'] = markers['DATA START'].split('\n') 
     # remove empty spaces
@@ -35,6 +39,15 @@ def main(inter_code):
     # start by doing the declarations
     declr_assembly = declarations(markers)
     complete_assembly += declr_assembly
+
+    # text functions split
+    main_list = find_main(markers)
+    unmain_list = find_unmain(markers)
+
+    # print(main_list, unmain_list)
+    # test the processing of functions
+    complete_assembly += process_main(main_list)
+    complete_assembly += process_unmain(unmain_list)
 
 
     extlibs_assembly = external_libs(markers)
@@ -102,10 +115,317 @@ def find_main(markers):
         else:
             unmain_list.append(term)
 
-    # test
-    print(unmain_list)
-    print(main_list)
+    # return main list
+    return main_list
 
-        
+def find_unmain(markers):
+    main_list = []
+    unmain_list = []
+
+    main_found = 0
+    for term in markers['TEXT START']:
+
+        if term == "main:":
+            main_found = 1
+
+        if main_found == 1:
+            if term == "func end":
+                main_list.append(term)
+                # change flag
+                main_found = 0
+            else:
+                main_list.append(term)
+
+        # else if flag and not in main just append to the unmain list
+        else:
+            unmain_list.append(term)
+
+    # return main list
+    return unmain_list
+
+    
+def process_main(main_list):
+    # tab status para saber si tengo que colocar el tab character '\t' en el label
+    tab_status = 0
+    tab_character = '\t'
+
+    formed_function = ''
+
+    for i in range(len(main_list)):
+    # for value in main_list:
+        if main_list[i] =='main:':
+            # add to formed function
+            formed_function += '\n' + str(main_list[i])
+            # change the tab status
+            tab_status = 1
+
+        else:
+            # else if not the tag main:
+            # could be the end or the beginning
+            if main_list[i] == 'func start':
+                tab_status = 1
+            if main_list[i] == 'func end':
+                tab_status = 0
+
+            # could be anyting else
+            # IF
+            if 'if' in main_list[i]:
+                # its an if remove the starting 3
+                temp = main_list[i][3:]
+                # split based on the jump
+                temp = temp.split(' jump ')
+                # temp1 has the register temp2 has the location
+                formed_function += '\n' + tab_status*tab_character + 'cmp ' + temp[0] + ', #1'
+                formed_function += '\n' + tab_status*tab_character + 'beq ' + temp[1]
+
+            # its an assigment
+            if '=' in main_list[i]:
+                # could be an assigment of a function
+                if 'func_call' in main_list[i]:
+                    # split
+                    temp = main_list[i].split(' func_call ')
+
+                    # temp1 has the register and =, temp2 has the function
+                    formed_function += '\n' + tab_status*tab_character + 'b ' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'ldr ' + temp[0][:-1] + ', #r0'
+                    continue
+
+                # division
+                if '/' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('/')
+                    formed_function += '\n' + tab_status*tab_character + 'mov ' + temp[0] + ', ' + without_div[0] + ', lsr 1'
+                    continue
+
+                if '%' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('%')
+                    formed_function += '\n' + tab_status*tab_character + 'mov ' + temp[0] + ', ' + without_div[0] + ' mod '+ without_div[1]
+                    continue
+
+                if '+' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('+')
+                    formed_function += '\n' + tab_status*tab_character + 'add ' + temp[0] + ', ' + without_div[0] + ', '+ without_div[1]
+                    continue
+
+                if '-' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('-')
+                    formed_function += '\n' + tab_status*tab_character + 'sub ' + temp[0] + ', ' + without_div[0] + ', '+ without_div[1]
+                    continue
+
+                if '!=' in main_list[i]:
+                    temp = main_list[i].split('!=')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1]+ ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movne ' + left_right[0] + ', #1'
+                    continue
+
+                if '==' in main_list[i]:
+                    temp = main_list[i].split('==')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'moveq ' + left_right[0] + ', #1'
+                    continue
+
+                if '>' in main_list[i]:
+                    temp = main_list[i].split('>')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1]  + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movgt ' + left_right[0] + ', #1'
+                    continue
+
+                if '<' in main_list[i]:
+                    temp = main_list[i].split('<')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movlt ' + left_right[0] + ', #1'
+                    continue
+
+                if '>=' in main_list[i]:
+                    temp = main_list[i].split('>=')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movge ' + left_right[0] + ', #1'
+                    continue
+
+                if '<=' in main_list[i]:
+                    temp = main_list[i].split('<=')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movle ' + left_right[0] + ', #1'
+                    continue
+
+                # ASIGNACION si completo todo lo demas
+                temp = main_list[i].split('=')
+                right = temp[1]
+                # fix bug of assignment of function
+                if temp[1] == '0':
+                    # go back one iteration and retrieve the left side of the equation
+                    prev_func = main_list[i-1].split('=')
+                    right = prev_func[0]
+
+                formed_function += '\n' + tab_status*tab_character + 'mov ' + temp[0] + ', ' + right
 
 
+    return formed_function
+
+
+def process_unmain(main_list):
+    # tab status para saber si tengo que colocar el tab character '\t' en el label
+    tab_status = 0
+    tab_character = '\t'
+
+    formed_function = ''
+
+    for i in range(len(main_list)):
+    # for value in main_list:
+        if ':' in main_list[i]:
+            # add to formed function
+            formed_function += '\n' + str(main_list[i])
+            # change the tab status
+            tab_status = 1
+
+        else:
+            # else if not the tag main:
+            # could be the end or the beginning
+            if main_list[i] == 'func start':
+                tab_status = 1
+            if main_list[i] == 'func end':
+                tab_status = 0
+
+            # could be anyting else
+            # IF
+            if 'if' in main_list[i]:
+                # its an if remove the starting 3
+                temp = main_list[i][3:]
+                # split based on the jump
+                temp = temp.split(' jump ')
+                # temp1 has the register temp2 has the location
+                formed_function += '\n' + tab_status*tab_character + 'cmp ' + temp[0] + ', #1'
+                formed_function += '\n' + tab_status*tab_character + 'beq ' + temp[1]
+
+            # its an assigment
+            if '=' in main_list[i]:
+                # could be an assigment of a function
+                if 'func_call' in main_list[i]:
+                    # split
+                    temp = main_list[i].split(' func_call ')
+
+                    # temp1 has the register and =, temp2 has the function
+                    formed_function += '\n' + tab_status*tab_character + 'b ' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'ldr ' + temp[0][:-1] + ', #r0'
+                    continue
+
+                # division
+                if '/' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('/')
+                    formed_function += '\n' + tab_status*tab_character + 'mov ' + temp[0] + ', ' + without_div[0] + ', lsr 1'
+                    continue
+
+                if '%' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('%')
+                    formed_function += '\n' + tab_status*tab_character + 'mov ' + temp[0] + ', ' + without_div[0] + ' mod '+ without_div[1]
+                    continue
+
+                if '+' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('+')
+                    formed_function += '\n' + tab_status*tab_character + 'add ' + temp[0] + ', ' + without_div[0] + ', '+ without_div[1]
+                    continue
+
+                if '-' in main_list[i]:
+                    # split by the equal
+                    temp = main_list[i].split('=')
+                    without_div = temp[1].split('-')
+                    formed_function += '\n' + tab_status*tab_character + 'sub ' + temp[0] + ', ' + without_div[0] + ', '+ without_div[1]
+                    continue
+
+                if '!=' in main_list[i]:
+                    temp = main_list[i].split('!=')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1]+ ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movne ' + left_right[0] + ', #1'
+                    continue
+
+                if '==' in main_list[i]:
+                    temp = main_list[i].split('==')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'moveq ' + left_right[0] + ', #1'
+                    continue
+
+                if '>' in main_list[i]:
+                    temp = main_list[i].split('>')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1]  + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movgt ' + left_right[0] + ', #1'
+                    continue
+
+                if '<' in main_list[i]:
+                    temp = main_list[i].split('<')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movlt ' + left_right[0] + ', #1'
+                    continue
+
+                if '>=' in main_list[i]:
+                    temp = main_list[i].split('>=')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movge ' + left_right[0] + ', #1'
+                    continue
+
+                if '<=' in main_list[i]:
+                    temp = main_list[i].split('<=')
+                    # temp0 has the left side and temp1 the left side of the inequity
+                    left_right = temp[0].split('=')
+                    #
+                    formed_function += '\n' + tab_status*tab_character + 'cmp ' + left_right[1] + ', #' + temp[1]
+                    formed_function += '\n' + tab_status*tab_character + 'movle ' + left_right[0] + ', #1'
+                    continue
+
+                # ASIGNACION si completo todo lo demas
+                temp = main_list[i].split('=')
+                right = temp[1]
+                # fix bug of assignment of function
+                if temp[1] == '0':
+                    # go back one iteration and retrieve the left side of the equation
+                    prev_func = main_list[i-1].split('=')
+                    right = prev_func[0]
+                formed_function += '\n' + tab_status*tab_character + 'mov ' + temp[0] + ', ' + right
+
+
+    return formed_function
